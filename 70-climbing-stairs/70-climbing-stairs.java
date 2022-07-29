f2fa1fa45a2e025f1c5fa5ec23c55810c497cdfa
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-07-29 12:20:08 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (65.22%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,17 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
         
-        return climb(n, new int[n+1]);
+        // return usingMemoization(n, new int[n+1]);
+        return UsingTabulation(n, new int [n+1]);
     }
-    int climb(int n, int[] dp){
+    int usingMemoization(int n, int[] dp){
         if(n==0) return 1;
         if(n<0) return 0;
         if(dp[n]!=0) return dp[n];
         
-      int f = climb(n-1,dp);
-      int s = climb(n-2,dp);
+      int f = usingMemoization(n-1,dp);
+      int s = usingMemoization(n-2,dp);
         int ans =f+s;
         dp[n] = ans;
         return ans;
+    }
+    int UsingTabulation(int n, int[] dp){
+        
+        dp[0]=1;
+        for(int i=1; i<=n; i++){
+            if(i==1){
+             dp[i] = dp[i-1];  
+            }
+            else{
+                dp[i] = dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[n];
     }
 }
