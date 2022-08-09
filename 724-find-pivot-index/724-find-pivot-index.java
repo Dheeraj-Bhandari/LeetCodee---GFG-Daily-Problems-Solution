1dadf8fb902074266c71bc0368304539021f512c
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-09 12:52:50 +0530

    Time: 2 ms (79.27%), Space: 52.3 MB (54.73%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,15 +1,16 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         
-      long totalsum=0;
-        for(int i:nums) totalsum+=i;
-        long leftsum = 0;
+     long totalsum=0;
+     for(int i:nums) totalsum+=i;
+      long leftsum = 0;
         
         for(int i=0; i<nums.length; i++){
-            leftsum+=nums[i];
             
-            if(totalsum-leftsum==leftsum-nums[i]) return i;
+            leftsum+= nums[i];
+            
+            if(totalsum-leftsum == leftsum-nums[i]) return i;
         }
-      return -1;
+        return -1;
     }
 }
