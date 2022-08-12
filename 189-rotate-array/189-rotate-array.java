943925868ccb1092ae50f4993ffab55f3e925b22
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-12 16:52:50 +0530

    Update 189-rotate-array.java

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,22 +1,53 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        
+        // Brute Force Approach 
+//         if(k==nums.length) return;
+//         if(k>nums.length){
+//             k = Math.abs(k%nums.length);
+//         }
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for(int i=nums.length-k; i<nums.length; i++){
+//             list.add(nums[i]);
+//         }
+//         int m =nums.length-1;
+//         for(int i = nums.length-k-1; i>=0; i--){
+//             nums[m] = nums[i];
+//             m--;
+//         }
+//         for(int i = 0; i<k; i++){
+//             nums[i] = list.get(i);
+            
+//         }
+//         return;
+//     }
+// }
+    
+//         -----------------------
+//         Optimal Approach 
+        
+        
+        class Solution {
+    public void rotate(int[] nums, int k) {
+        int n =nums.length;
         if(k==nums.length) return;
         if(k>nums.length){
             k = Math.abs(k%nums.length);
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=nums.length-k; i<nums.length; i++){
-            list.add(nums[i]);
-        }
-        int m =nums.length-1;
-        for(int i = nums.length-k-1; i>=0; i--){
-            nums[m] = nums[i];
-            m--;
-        }
-        for(int i = 0; i<k; i++){
-            nums[i] = list.get(i);
-            
-        }
+        reverse(nums, 0, n-k-1);
+        reverse (nums, n-k, n-1 );
+        reverse (nums, 0 , n-1);
         return;
     }
+    void reverse(int[] arr, int a , int b){
+        while(a<b){
+            int  temp = arr[a];
+            arr[a]=arr[b];
+            arr[b]=temp;;
+            a++;
+            b--;
+        }
+    }
 }
+    
+    
