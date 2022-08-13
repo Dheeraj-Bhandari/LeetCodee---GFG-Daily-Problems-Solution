37a2f158ae60eaecb005070a0323d98026cafc16
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-13 14:22:51 +0530

    Time: 50 ms (72.76%), Space: 149.2 MB (59.23%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-     Deque<Integer> dq = new ArrayDeque<>();
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i=0;i<k; i++){
+    Deque<Integer> dq = new ArrayDeque<>();
+    ArrayList<Integer> list = new ArrayList<>();
+    for(int i=0; i<k; i++){
+       
             while(dq.size()>0 && dq.peekLast()<nums[i]){
                 dq.removeLast();
             }
-            dq.addLast(nums[i]);
-        }
         
-        list.add(dq.peekFirst());
-        for(int i=k; i<nums.length; i++){
-            if(dq.peekFirst()==nums[i-k])dq.removeFirst();
-            
-             while(dq.size()>0 && dq.peekLast()<nums[i]){
+        dq.addLast(nums[i]);
+    }
+    list.add(dq.peek());
+        for(int i=k ;i<nums.length; i++){
+            if(dq.peekFirst()==nums[i-k]) dq.removeFirst();
+            while(dq.size()>0 && dq.peekLast()<nums[i]){
                 dq.removeLast();
             }
-            dq.addLast(nums[i]);
-            list.add(dq.peekFirst());
+        
+        dq.addLast(nums[i]);
+        list.add(dq.peekFirst());
+            
         }
+        
         int[] ans = new int[list.size()];
         for(int i=0; i<list.size(); i++){
             ans[i] = list.get(i);
         }
-       
-          return ans;  
+        return ans;
     }
 }
