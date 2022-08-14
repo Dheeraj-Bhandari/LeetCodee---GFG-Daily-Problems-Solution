36972d531ad48a315d39088d278f6755c9d6b32e
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-14 14:19:13 +0530

    Time: 2 ms (82.89%), Space: 54.8 MB (53.31%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,17 +1,26 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length-1;
+//         int n = nums.length-1;
         
-         for(int  j=0; j<nums.length; j++){
-        for(int  i=0; i<nums.length-1; i++){
+//          for(int  j=0; j<nums.length; j++){
+//         for(int  i=0; i<nums.length-1; i++){
             
-            if(nums[i]==0){
-                int temp = nums[i+1];
-                nums[i+1] = nums[i];
-                nums[i] = temp;
+//             if(nums[i]==0){
+//                 int temp = nums[i+1];
+//                 nums[i+1] = nums[i];
+//                 nums[i] = temp;
                 
-             }
+//              }
+//             }
+//         }
+        
+        int pointer = 0;
+        for(int number:nums){
+            if(number!=0){
+                nums[pointer]=number;
+                pointer++;
             }
         }
+        while(pointer!=nums.length) nums[pointer++]=0;
     }
 }
