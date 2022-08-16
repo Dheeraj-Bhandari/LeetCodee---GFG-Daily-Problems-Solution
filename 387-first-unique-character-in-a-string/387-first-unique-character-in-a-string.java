7fa78f41c548b46d608452d253a4b630707d57b6
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-16 09:59:49 +0530

    Time: 23 ms (67.24%), Space: 42.8 MB (92.10%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,19 +1,27 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        char [] ch  = s.toCharArray();
-        for(char c :ch){
-            map.put(c, map.getOrDefault(c , 0)+1);
-        }
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         char [] ch  = s.toCharArray();
+//         for(char c :ch){
+//             map.put(c, map.getOrDefault(c , 0)+1);
+//         }
         
-       for(int i=0; i<ch.length; i++){
-           if(map.containsKey(ch[i])) {
-               if(map.get(ch[i])<=1){
-                   return i;
-               }
-           }
-       }
-        System.out.println(map);
-        return -1;
-    }
+//        for(int i=0; i<ch.length; i++){
+//            if(map.containsKey(ch[i])) {
+//                if(map.get(ch[i])==1){
+//                    return i;
+//                }
+//            }
+//        }
+        
+//         return -1;
+
+        
+        int ans =-1;
+        for(char c:s.toCharArray()){
+            if(s.indexOf(c)==s.lastIndexOf(c)) return s.indexOf(c);
+        }
+        return ans;
+}
+    
 }
