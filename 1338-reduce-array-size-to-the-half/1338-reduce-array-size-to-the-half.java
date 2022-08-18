deba86718954cfa05a22247f5afc28a23b2cafc4
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-18 13:04:33 +0530

    Time: 90 ms (57.37%), Space: 83 MB (72.10%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,18 +1,20 @@
 class Solution {
     public int minSetSize(int[] arr) {
+        
+        // Creating Map ans Stroing feq of each element;
+        
         HashMap<Integer, Integer> map = new HashMap<>();
-      
-
-
         for(int i=0; i<arr.length; i++){
             map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
             
         }
         
+        // Stroing frq value in ArrayList then sorting it in Reverse order so max frq came at top
         ArrayList<Integer> list = new ArrayList<>(map.values());
         Collections.sort(list, Collections.reverseOrder());
         
         
+        // Ruunig loop till len become <= the Half Size of Array. and finding max frq data in arraylist and subtracting its value in len Variable
         int i =0;
         int ans =0;
         int len = arr.length;
@@ -21,6 +23,8 @@ class Solution {
             i++;
             ans++;
         }
+        
+        
         return ans;
     }
 }
