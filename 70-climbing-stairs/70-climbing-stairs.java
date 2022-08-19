bb2296a2aab3dd963e2fdc89c0f8774785f642fe
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-19 11:08:46 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (51.78%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,31 +1,16 @@
 class Solution {
+    
     public int climbStairs(int n) {
-        
-        // return usingMemoization(n, new int[n+1]);
-        return UsingTabulation(n, new int [n+1]);
+      int [] dp = new int[n+1];
+        return climbStairs2(n, dp);
     }
-    int usingMemoization(int n, int[] dp){
+      int climbStairs2(int n, int[] dp) {
         if(n==0) return 1;
         if(n<0) return 0;
         if(dp[n]!=0) return dp[n];
-        
-      int f = usingMemoization(n-1,dp);
-      int s = usingMemoization(n-2,dp);
-        int ans =f+s;
+    int ans = climbStairs2(n-1, dp)+ climbStairs2(n-2, dp);
         dp[n] = ans;
         return ans;
     }
-    int UsingTabulation(int n, int[] dp){
-        
-        dp[0]=1;
-        for(int i=1; i<=n; i++){
-            if(i==1){
-             dp[i] = dp[i-1];  
-            }
-            else{
-                dp[i] = dp[i-1]+dp[i-2];
-            }
-        }
-        return dp[n];
-    }
+    
 }
