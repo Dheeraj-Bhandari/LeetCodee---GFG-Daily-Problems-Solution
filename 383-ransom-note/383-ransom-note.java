3147094c89f86df40edc93a6382611b10f1b2d46
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-25 10:32:05 +0530

    Time: 4 ms (84.52%), Space: 46.5 MB (55.75%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,18 +1,33 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map  =new HashMap<>();
+//         HashMap<Character,Integer> map  =new HashMap<>();
+//         char [] ch  = ransomNote.toCharArray();
+//         char [] ch2  = magazine.toCharArray();
+//         for(char c:ch2){
+//             map.put(c, map.getOrDefault(c, 0)+1);
+//         }
+        
+//         for(int i=0; i<ch.length; i++){
+//             if(map.containsKey(ch[i])){
+//                 if(map.get(ch[i])>0){
+//                     map.put(ch[i], map.getOrDefault(ch[i], 0)-1);
+//                 }
+//                 else return false;
+//             }
+//             else return false;
+//         }
+//         return true;
+        
+        int[] map  = new int[26];
         char [] ch  = ransomNote.toCharArray();
         char [] ch2  = magazine.toCharArray();
         for(char c:ch2){
-            map.put(c, map.getOrDefault(c, 0)+1);
+            map[c-'a']++;
         }
-        
         for(int i=0; i<ch.length; i++){
-            if(map.containsKey(ch[i])){
-                if(map.get(ch[i])>0){
-                    map.put(ch[i], map.getOrDefault(ch[i], 0)-1);
-                }
-                else return false;
+            if(map[ch[i]-'a']>0){
+               map[ch[i]-'a']--;
+                
             }
             else return false;
         }
