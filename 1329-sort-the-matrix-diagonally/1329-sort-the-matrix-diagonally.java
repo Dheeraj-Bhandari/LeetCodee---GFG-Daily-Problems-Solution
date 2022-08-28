927c1e8e568cbecf27e72a2b0321dc53ac487c2b
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-28 11:29:41 +0530

    Time: 1 ms (100.00%), Space: 48.3 MB (34.98%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        //Sort First Half of matrix diagnal.
+        for(int i=0; i<m; i++){
+            sortmatrix(mat, i, 0);
+        }
+         //Sort Second Half of matrix diagnal.
+        for(int j=1; j<n; j++){
+            sortmatrix(mat, 0, j);
+        }
+        return mat;
+    }
+    
+    void sortmatrix(int[][] mat, int r, int c){
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        int i=r;
+        int j=c;
+        
+        int[] map = new int[101];
+        //fill map with matrix frq
+        
+        while(i<m && j<n ){
+            map[mat[i][j]]++;
+            i++;
+            j++;
+        }
+        
+        i=r;
+        j=c;
+        
+        for(int k=1; k<map.length; k++){
+            while(map[k]>0){
+                mat[i][j]= k;
+                map[k]--;
+                i++;
+                j++;
+            }
+        }
+        // return mat;
+    }
+}
