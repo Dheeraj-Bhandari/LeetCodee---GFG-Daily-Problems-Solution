82cb30f00c6fef66aa2701b8832e61e5c92f7c42
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-28 16:41:02 +0530

    Time: 1 ms (50.74%), Space: 41.9 MB (78.05%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -13,17 +13,47 @@
  *     }
  * }
  */
+
+// Recursively 
+
+// class Solution {
+//     List<Integer> list = new ArrayList<>();
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         if(root ==null){
+//             return list;
+//         }
+//         list.add(root.val);
+//          preorderTraversal(root.left);
+//         preorderTraversal(root.right);
+        
+//         return list;
+        
+//     }
+// }
+
+//Itratively
+
 class Solution {
-    List<Integer> list = new ArrayList<>();
+ 
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root ==null){
-            return list;
+     Stack<TreeNode>  stack = new Stack<>();
+     List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        
+        stack.push(root);
+        
+        while(stack.size()>0){
+            TreeNode temp  =stack.pop();
+            list.add(temp.val);
+            if(temp.right!=null){
+                stack.push(temp.right);
+            }
+            if(temp.left!=null){
+                stack.push(temp.left);
+            }
+            
         }
-        list.add(root.val);
-         preorderTraversal(root.left);
-        preorderTraversal(root.right);
         
         return list;
-        
     }
 }
