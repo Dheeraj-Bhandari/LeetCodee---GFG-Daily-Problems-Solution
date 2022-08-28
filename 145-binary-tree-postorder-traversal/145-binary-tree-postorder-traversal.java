5edd626bffcc2c618bef3d79a27e5b6069e26154
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-28 18:31:16 +0530

    Time: 1 ms (52.02%), Space: 42.2 MB (51.27%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,13 +13,34 @@
  *     }
  * }
  */
+// class Solution {
+//     List<Integer> list = new ArrayList<>();
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         if(root==null) return list;
+//         postorderTraversal(root.left);
+//         postorderTraversal(root.right);
+//         list.add(root.val);
+//         return list;
+//     }
+// }
+
 class Solution {
-    List<Integer> list = new ArrayList<>();
+    
     public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
         if(root==null) return list;
-        postorderTraversal(root.left);
-        postorderTraversal(root.right);
-        list.add(root.val);
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack.push(root);
+       while(!stack.isEmpty()){
+           TreeNode node =stack.pop();
+           stack2.push(node);
+           if(node.left!=null) stack.push(node.left);
+           if(node.right!=null) stack.push(node.right);
+       }
+        while(stack2.size()>0){
+            list.add(stack2.pop().val);
+        }
         return list;
     }
 }
