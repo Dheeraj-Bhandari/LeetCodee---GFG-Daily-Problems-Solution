4f10d5c53ecdd2d0195b59a927305a24439112c1
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-29 10:52:27 +0530

    Time: 5 ms (55.04%), Space: 57.2 MB (59.85%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,33 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m=grid.length;
+        int m = grid.length;
         int n = grid[0].length;
-        int   max = 0;
+        int max = 0;
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]=='1'){
-                   
-                    max+=func(grid, i,j, m, n);
-                    
+                   if(check(grid, i, j, m, n)==true){
+                       max++;
+                   }
                 }
             }
         }
         return max;
-        
     }
-    
-    int func(char[][] grid, int i, int j , int m , int n){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] =='0') return 0;
+    private boolean check(char[][] grid, int i, int j, int m, int n){
+        if(i>=m || i<0 || j>=n || j<0 || grid[i][j]=='0') return false;
         
-        grid[i][j] = '0';
+        grid[i][j] ='0';
         
-        int down = func(grid, i+1, j, m, n);
-        int right = func(grid, i, j+1, m, n);
-        int top = func(grid, i-1, j, m, n);
-        int left = func(grid, i, j-1, m, n);
-
-        return 1;
-        
+        check(grid, i-1, j,m,n);
+        check(grid, i+1, j, m,n);
+        check(grid, i, j-1 ,m ,n);
+        check(grid, i, j+1, m, n);
+        return true;
         
     }
 }
