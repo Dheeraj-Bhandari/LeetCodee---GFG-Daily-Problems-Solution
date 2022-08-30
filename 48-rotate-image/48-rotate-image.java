2880cbb7cacb9aacd01f2849692a765c2fc1b37c
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-08-30 11:35:40 +0530

    Time: 0 ms (100.00%), Space: 41 MB (93.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -3,25 +3,54 @@ class Solution {
         int r = matrix.length;
         int c = matrix[0].length;
         
-        int[][] resultmat = new int[r][c];
+//         int[][] resultmat = new int[r][c];
+            
+//             int j=0;
+//             int k=0;
+//             while(j<c){
+//             for( int i=r-1; i>=0; i--){
+//                 resultmat[j][k] = matrix[i][j];
+//                 k++;
+//              }
+//                 j++; 
+//                 k=0;   
+//         }
+//         for(int m=0; m<r; m++){
+//             for(int n=0; n<c; n++){
+//                 matrix[m][n] = resultmat[m][n];
+//             }
+//         }
+        
+        transpose(matrix, r, c);
+        
+        swap(matrix,r,c);
+    
             
         
-             int j=0;
-            int k=0;
-            while(j<c){
-            for( int i=r-1; i>=0; i--){
-                resultmat[j][k] = matrix[i][j];
-                k++;
-             }
-                j++; 
-                k=0;
-                
-        }
-       
-        for(int m=0; m<r; m++){
-            for(int n=0; n<c; n++){
-                matrix[m][n] = resultmat[m][n];
+    }
+    void transpose(int[][] matrix, int r, int c){
+        for(int i =0; i<r; i++){
+            for(int j=i+1; j<c; j++){
+                int temp  =matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
+    }
+    
+    void swap(int[][] matrix, int r, int c){
+        int lastcol = r-1;
+        int firstcol= 0;
+       while(firstcol<lastcol){
+           
+           for(int i=0; i<r; i++){
+               int temp = matrix[i][firstcol];
+               matrix[i][firstcol] = matrix[i][lastcol];
+               matrix[i][lastcol]= temp;
+           }
+           lastcol--;
+           firstcol++;
+           
+       }
     }
 }
