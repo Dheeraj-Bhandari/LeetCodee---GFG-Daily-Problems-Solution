6753b9f9bb32170dab93c6ee05ab96afebff5555
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-09-13 13:09:48 +0530

    Time: 5 ms (51.05%), Space: 44.5 MB (17.73%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,42 +1,24 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
+     
+        HashMap<Integer, Integer> map = new HashMap<>();
+     
         for(int i:nums1){
-            map1.put(i, map1.getOrDefault(i, 0)+1);
+            map.put(i, map.getOrDefault(i, 0)+1);
         }
+        
         for(int i:nums2){
-            map2.put(i, map2.getOrDefault(i, 0)+1);
-        }
-        System.out.println(map1);
-        System.out.println(map2);
-        Set<Integer>  set = new HashSet<>();
-        for(int i:nums1){
-            if(map2.containsKey(i) && map1.containsKey(i) && !set.contains(i)){
-                 if(map2.get(i)>=map1.get(i)){
-                     int t = map1.get(i);
-                while(t-->0){
-                    ans.add(i);
-                        }
-                     set.add(i);
-                  }
-                else if(map1.get(i)>=map2.get(i)){
-                    int t = map2.get(i);
-                while(t-->0){
-                    ans.add(i);
-                    }
-                    set.add(i);
-                }
+            if(map.containsKey(i) && map.get(i)>0){
+                ans.add(i);
+                map.put(i, map.get(i)-1);
             }
-          
-            
-            
         }
-        int[] arr = new int[ans.size()];
+        int[]arr = new int[ans.size()];
         for(int i=0; i<arr.length; i++){
             arr[i] = ans.get(i);
         }
         return arr;
-    }
+       
+    }   
 }
