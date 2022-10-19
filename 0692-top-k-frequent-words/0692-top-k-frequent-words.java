cce26c08fa515d59b5652c9f5ed62f384da93f73
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-10-20 00:19:59 +0530

    Time: 12 ms (58.40%), Space: 45.6 MB (24.62%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> res = new LinkedList<>(); 
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < words.length; i++){
+            if(map.containsKey(words[i])){
+                map.put(words[i], map.get(words[i])+ 1);
+            }
+            else{
+                map.put(words[i], 1);
+            }
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>>pq = new PriorityQueue<>(
+            (a,b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) :a.getValue() - b.getValue()
+        );
+
+        for (Map.Entry<String, Integer> entry: map.entrySet()){
+            pq.offer(entry);
+            if(pq.size()> k) pq.poll();
+        }
+
+        while(!pq.isEmpty()){
+            res.add(0, pq.poll().getKey()); 
+        }
+
+        return res;
+    }
+}
