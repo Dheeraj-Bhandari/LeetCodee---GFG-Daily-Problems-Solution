761df79447a7e9e4edf350b546b79ad2439a0f97
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-10-20 11:45:23 +0530

    Time: 5 ms (94.02%), Space: 41.7 MB (97.33%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,15 +3,15 @@ class Solution {
         String[] strRoman = {"M","CM", "D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
         
         for(int i=0; i<13; i++){
             while(num-val[i]>=0){
-                ans+=strRoman[i];
+                ans.append(strRoman[i]);
                 num-=val[i];
             }
             
         }
-        return ans;
+        return ans.toString();
     }
 }
