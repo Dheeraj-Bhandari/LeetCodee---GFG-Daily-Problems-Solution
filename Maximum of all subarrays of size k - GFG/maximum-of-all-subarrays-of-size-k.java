64ec0b48b24fa44829fa168d73450e030b1ce96e
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-10-25 12:20:46 +0530

    Added solution - LeetHub

---
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        // Your code here
+        int max = Integer.MIN_VALUE;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        int j=0;
+        for(int i=0; i<arr.length; i++){
+            if(pq.size()<k)pq.add(arr[i]);
+           
+            if(pq.size()==k){
+            list.add(pq.peek());
+            pq.remove(arr[j]);
+            j++;
+            }    
+              
+            
+        }
+       
+        return list;
+    }
+}
