7b0678e01edfe1cfa59a6c91d32f33a6c5a6f4ac
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-11-03 13:16:26 +0530

    Time: 349 ms (22.17%), Space: 125 MB (26.12%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,52 +1,26 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        if(words.length==0) return 0;
         
-        Set<String> visited = new HashSet<>();
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(String s:words){
-            map.put(s,map.getOrDefault(s,0)+1);
-        }
-        
-        int length = 0;
-        boolean singlePairString = false;
+        HashMap<String , Integer> map = new HashMap<>();
+        int ans = 0;
         for(String str:words){
-            String reverseStr = reverse(str);
             
-            if(map.containsKey(str)&& map.containsKey(reverseStr) &&  !visited.contains(str) && !visited.contains(reverseStr) && !str.equals(reverseStr)){
-                int strCount = (map.get(str)) ;
-                int reverseStrCount = map.get(reverseStr) ;
-                length += (Math.min(strCount, reverseStrCount))*4;
-                
-                visited.add(str);
-                visited.add(reverseStr);
+            String reverse = ""+str.charAt(1)+str.charAt(0);
+            if(map.containsKey(reverse)){
+                ans+=4;
+                map.put(reverse, map.get(reverse)-1);
+                if(map.get(reverse)==0)map.remove(reverse);
             }
-            else if(str.equals(reverseStr) && !visited.contains(str)){
-               int strCount = (map.get(str)) ;
-                if(strCount%2==0){
-                    length+=strCount*2;
-                }
-                else{
-                    strCount-=1;
-                    length+=strCount*2;
-                    singlePairString= true;
-                }
-                visited.add(str);
-            }
-            
-            
+            else map.put(str, map.getOrDefault (str,0) +1);
         }
         
-        return singlePairString? length+2 : length ;
-    }
-    String reverse(String str){
-        StringBuilder sb = new StringBuilder();
-        for(int i=str.length()-1; i>=0; i--){
-            sb.append(str.charAt(i));
-            
+        for(char i='a'; i<='z'; i++){
+            String str = ""+i+i;
+           if(map.containsKey(str) && map.get(str)>0){
+               ans+=2;
+               break;
+           }
         }
-        return sb.toString();
+        return ans;
     }
 }
