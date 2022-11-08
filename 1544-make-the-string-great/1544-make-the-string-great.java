e40225a4208a6a3f53e0fd58127d3202ea3ab0ad
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-11-08 12:17:00 +0530

    Time: 3 ms (93.08%), Space: 43.2 MB (23.44%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,26 +1,24 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> stack = new Stack<>();
+       
+        StringBuilder sb = new StringBuilder(s);
         
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(!stack.isEmpty() && Character.isUpperCase(ch) && Character.isLowerCase(stack.peek())
-               && Character.toLowerCase(ch)==(stack.peek())){
-                stack.pop();
+        for(int i =0; i<sb.length()-1; i++){
+                char ch = sb.charAt(i);
+                char ch2 = sb.charAt(i+1);
+            
+            if(Math.abs((int)ch - (int)ch2)==32){
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                
+                if(i==0) i--;
+                else i-=2;
             }
-            else if(!stack.isEmpty() && Character.isLowerCase(ch) && Character.isUpperCase(stack.peek())
-                    && Character.toLowerCase(stack.peek())==(ch)){
-                stack.pop();
-            }
-            else stack.push(ch);
+            if(sb.length()==0) return "";
+           
+            
         }
-        char[] ch = new char[stack.size()];
-        int i = stack.size()-1;
-        while(stack.size()>0){
-            ch[i]= stack.peek();
-            stack.pop();
-            i--;
-        }
-        return String.valueOf(ch);
+        return sb.toString();
+        
     }
 }
