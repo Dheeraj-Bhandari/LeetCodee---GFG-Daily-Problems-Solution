7581708c9c920e7e5727c350d757592e611462d7
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2022-12-17 09:35:42 +0530

    Time: 7 ms (81.80%), Space: 41.9 MB (93.92%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -27,7 +27,7 @@ class Solution {
                 
             }
             else{
-                ;
+                
                 int value= Integer.parseInt(s);
                 stack.push(value);
                 
