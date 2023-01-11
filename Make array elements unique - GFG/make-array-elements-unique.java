2b93a191e7144c9e90dfbf36628f44dc9ea0db93
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2023-01-11 14:23:25 +0530

    Added solution - LeetHub

--- Make array elements unique - GFG/make-array-elements-unique.java
+++ Make array elements unique - GFG/make-array-elements-unique.java
@@ -40,7 +40,7 @@ class Solution {
         // Code here
       Arrays.sort(arr);
       
-      long ans =0;
+      int ans =0;
       
       for(int i=0; i<N-1; i++){
             if(arr[i]>=arr[i+1]){
