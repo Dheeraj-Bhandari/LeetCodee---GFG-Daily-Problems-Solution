e4f3e85ff68864dde59c3406c51e09ed980459eb
Dheeraj Bhandari <62802431+Dheeraj-Bhandari@users.noreply.github.com>
2023-01-11 13:55:42 +0530

    Added solution - LeetHub

---
+++ Make array elements unique - GFG/make-array-elements-unique.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            
+            int i = 0;
+            int N = Integer.parseInt(br.readLine().trim());
+        
+            int arr[] = new int[(int)(N)];
+            
+            String inputLine2[] = br.readLine().trim().split(" ");
+            for ( i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(inputLine2[i]);
+            }
+        
+            Solution ob = new Solution();
+            System.out.println(ob.minIncrements(arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public long minIncrements(int[] arr, int N) {
+        // Code here
+      Arrays.sort(arr);
+      
+      int ans =0;
+      
+      for(int i=0; i<N-1; i++){
+            // if(arr[i]==arr[i+1]){
+            //     arr[i+1]=arr[i+1]+1;
+            //     ans++;
+            // }
+            if(arr[i]>=arr[i+1]){
+                
+                while(arr[i]>=arr[i+1]){
+                 
+                    arr[i+1] = arr[i+1]+1;
+                    ans++;
+                }
+            }
+      }
+      return ans;
+        
+    }
+}
